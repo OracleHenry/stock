f1769b297386ddc57c97a4d0ff0df4c3618a19ff
OracleHenry <henry_yin@139.com>
2017-06-14 11:12:29 +0800

    C3尝试只打印股票代码，前1000
    C3尝试只打印股票代码，前1000

--- C3.py
+++ C3.py
@@ -10,7 +10,7 @@ names=pd.DataFrame(ts.get_stock_basics())
 goodlist=[]
 
 
-for c in codes[100:199]:
+for c in codes[0:1000]:
         good = True
         df = pd.DataFrame(ts.get_hist_data(code=str(c), start='2017-06-07', end='2017-06-13')).fillna(-1)
         p_change = list(df['p_change'])
@@ -23,17 +23,19 @@ for c in codes[100:199]:
             if str(c).startswith('002') or str(c).startswith('30'):
                 continue
             else:
-                goodlist.append(c)
+                print(c)
+                #goodlist.append(c)
+            
         else:
             continue
 
 #######################
 
 
-names = names.reindex(goodlist)
-for n in names['name']:
-        if str(n).lower().startswith('n'):
-            continue
-        else:
-            print(n)
+#names = names.reindex(goodlist)
+#for n in names['name']:
+#        if str(n).lower().startswith('n'):
+#            continue
+#        else:
+#            print(n)
 
