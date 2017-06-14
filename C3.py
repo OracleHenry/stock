36b6a8a1bc4a2e6f5e9d5754cd4f878f440bf6c0
OracleHenry <henry_yin@139.com>
2017-06-15 01:59:15 +0800

    终于好了，写成excel就O了
    终于好了，写成excel就O了

--- C3.py
+++ C3.py
@@ -1,16 +1,19 @@
 #coding:utf-8
-
+import xlrd
+import openpyxl
+import xlwt
 import tushare as ts
 import pandas as pd
-
+import os,sys,string
 df=pd.DataFrame(ts.get_stock_basics())
 codes=df.index
 names=pd.DataFrame(ts.get_stock_basics())
 
 goodlist=[]
+goodlist1=[]
 
 try:
-    for c in codes:
+    for c in codes[0:10]:
         good = True
         df = pd.DataFrame(ts.get_hist_data(code=str(c), start='2017-06-13', end='2017-06-14')).dropna()
         if not df.empty:
@@ -27,13 +30,19 @@ try:
                     else:
                         #print(c)
                         goodlist.append(c)
+                        #goodlist1.append(str(''.join(' '.join(str(c)).split())))
+                        
 
                 else:
                     continue
 except(Exception) as e:
     print(e)
 else:
-    print((names.ix[goodlist])[['name','industry']])
+   res= (names.ix[goodlist])[['name','industry']]
+   #res['code']=goodlist1
+   #res.to_csv('d:\\Temp\\stock.csv',encoding="utf_8_sig")
+   res.to_excel('d:\\Temp\\stock1.xls',sheet_name='fuck')
+    
    
 
 
